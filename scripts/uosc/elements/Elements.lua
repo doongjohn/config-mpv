babc93bcd962efcb7789f1bdb7319721125089c1
doongjohn <doongjohn@gmail.com>
2023-11-10 11:46:18 +0900

    update

--- scripts/uosc/elements/Elements.lua
+++ scripts/uosc/elements/Elements.lua
@@ -1,4 +1,4 @@
-local Elements = {itable = {}}
+local Elements = {_all = {}}
 
 ---@param element Element
 function Elements:add(element)
@@ -9,8 +9,11 @@ function Elements:add(element)
 
 	if self:has(element.id) then Elements:remove(element.id) end
 
-	self.itable[#self.itable + 1] = element
+	self._all[#self._all + 1] = element
 	self[element.id] = element
+
+	-- Sort by render order
+	table.sort(self._all, function(a, b) return a.render_order < b.render_order end)
 
 	request_render()
 end
@@ -22,47 +25,30 @@ function Elements:remove(idOrElement)
 	if element then
 		if not element.destroyed then element:destroy() end
 		element.enabled = false
-		self.itable = itable_remove(self.itable, self[id])
+		self._all = itable_delete_value(self._all, self[id])
 		self[id] = nil
 		request_render()
 	end
 end
 
 function Elements:update_proximities()
-	local capture_mbtn_left = false
-	local capture_wheel = false
-	local menu_only = Elements.menu ~= nil
+	local curtain_render_order = Elements.curtain.opacity > 0 and Elements.curtain.render_order or 0
 	local mouse_leave_elements = {}
 	local mouse_enter_elements = {}
 
-	-- Calculates proximities and opacities for defined elements
+	-- Calculates proximities for all elements
 	for _, element in self:ipairs() do
 		if element.enabled then
 			local previous_proximity_raw = element.proximity_raw
 
-			-- If menu is open, all other elements have to be disabled
-			if menu_only then
-				if element.ignores_menu then
-					capture_mbtn_left = true
-					capture_wheel = true
-					element:update_proximity()
-				else
-					element.proximity_raw = infinity
-					element.proximity = 0
-				end
+			-- If curtain is open, we disable all elements set to rendered below it
+			if not element.ignores_curtain and element.render_order < curtain_render_order then
+				element:reset_proximity()
 			else
 				element:update_proximity()
 			end
 
-			-- Element has global forced key listeners
-			if element.on_global_mbtn_left_down then capture_mbtn_left = true end
-			if element.on_global_wheel_up or element.on_global_wheel_down then capture_wheel = true end
-
 			if element.proximity_raw == 0 then
-				-- Element has local forced key listeners
-				if element.on_mbtn_left_down then capture_mbtn_left = true end
-				if element.on_wheel_up or element.on_wheel_up then capture_wheel = true end
-
 				-- Mouse entered element area
 				if previous_proximity_raw ~= 0 then
 					mouse_enter_elements[#mouse_enter_elements + 1] = element
@@ -76,10 +62,6 @@ function Elements:update_proximities()
 		end
 	end
 
-	-- Enable key group captures requested by elements
-	mp[capture_mbtn_left and 'enable_key_bindings' or 'disable_key_bindings']('mbtn_left')
-	mp[capture_wheel and 'enable_key_bindings' or 'disable_key_bindings']('wheel')
-
 	-- Trigger `mouse_leave` and `mouse_enter` events
 	for _, element in ipairs(mouse_leave_elements) do element:trigger('mouse_leave') end
 	for _, element in ipairs(mouse_enter_elements) do element:trigger('mouse_enter') end
@@ -88,8 +70,15 @@ end
 -- Toggles passed elements' min visibilities between 0 and 1.
 ---@param ids string[] IDs of elements to peek.
 function Elements:toggle(ids)
-	local has_invisible = itable_find(ids, function(id) return Elements[id] and Elements[id].min_visibility ~= 1 end)
+	local has_invisible = itable_find(ids, function(id) return Elements[id] and Elements[id]:get_visibility() ~= 1 end)
 	self:set_min_visibility(has_invisible and 1 or 0, ids)
+	-- Reset proximities when toggling off. Has to happen after `set_min_visibility`,
+	-- as that is using proximity as a tween starting point.
+	if not has_invisible then
+		for _, id in ipairs(ids) do
+			if Elements[id] then Elements[id]:reset_proximity() end
+		end
+	end
 end
 
 -- Set (animate) elements' min visibilities to passed value.
@@ -98,14 +87,17 @@ end
 function Elements:set_min_visibility(visibility, ids)
 	for _, id in ipairs(ids) do
 		local element = Elements[id]
-		if element then element:tween_property('min_visibility', element.min_visibility, visibility) end
+		if element then
+			local from = math.max(0, element:get_visibility())
+			element:tween_property('min_visibility', from, visibility)
+		end
 	end
 end
 
 -- Flash passed elements.
 ---@param ids string[] IDs of elements to peek.
 function Elements:flash(ids)
-	local elements = itable_filter(self.itable, function(element) return itable_index_of(ids, element.id) ~= nil end)
+	local elements = itable_filter(self._all, function(element) return itable_index_of(ids, element.id) ~= nil end)
 	for _, element in ipairs(elements) do element:flash() end
 end
 
@@ -118,37 +110,34 @@ end
 -- Disabled elements don't receive these events.
 ---@param name string Event name.
 function Elements:proximity_trigger(name, ...)
-	for _, element in self:ipairs() do
+	for i = #self._all, 1, -1 do
+		local element = self._all[i]
 		if element.enabled then
-			if element.proximity_raw == 0 then element:trigger(name, ...) end
-			element:trigger('global_' .. name, ...)
+			if element.proximity_raw == 0 then
+				if element:trigger(name, ...) == 'stop_propagation' then break end
+			end
+			if element:trigger('global_' .. name, ...) == 'stop_propagation' then break end
 		end
 	end
 end
 
-function Elements:has(id) return self[id] ~= nil end
-function Elements:ipairs() return ipairs(self.itable) end
-
----@param name string Event name.
-function Elements:create_proximity_dispatcher(name)
-	return function(...) self:proximity_trigger(name, ...) end
+-- Returns a property of an element with a passed `id` if it exists, with an optional fallback.
+---@param id string
+---@param prop string
+---@param fallback any
+function Elements:v(id, prop, fallback)
+	if self[id] and self[id].enabled and self[id][prop] ~= nil then return self[id][prop] end
+	return fallback
 end
 
-mp.set_key_bindings({
-	{
-		'mbtn_left',
-		Elements:create_proximity_dispatcher('mbtn_left_up'),
-		function(...)
-			update_mouse_pos(nil, mp.get_property_native('mouse-pos'), true)
-			Elements:proximity_trigger('mbtn_left_down', ...)
-		end,
-	},
-	{'mbtn_left_dbl', 'ignore'},
-}, 'mbtn_left', 'force')
+-- Calls a method on an element with passed `id` if it exists.
+---@param id string
+---@param method string
+function Elements:maybe(id, method, ...)
+	if self[id] then return self[id]:maybe(method, ...) end
+end
 
-mp.set_key_bindings({
-	{'wheel_up', Elements:create_proximity_dispatcher('wheel_up')},
-	{'wheel_down', Elements:create_proximity_dispatcher('wheel_down')},
-}, 'wheel', 'force')
+function Elements:has(id) return self[id] ~= nil end
+function Elements:ipairs() return ipairs(self._all) end
 
 return Elements
